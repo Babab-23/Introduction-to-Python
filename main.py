455e0a929a8e94e24ab885525e957571b00d67dd
Babab-23 <adrianidialu@gmail.com>
2024-11-26 21:03:51 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+print("Welcome to 2024/2025 UEFA Champions League")
+print("Welcome to Codingal")
+#Champions league is one of the best leagues in international football
+#Codingal is a really fun experience which tells you all about python,HTML and C++
+# Printing a greeting message
+print("Hello,Codingal!")
+#Printing my name
+print("My name is Adrian.")
+#Printing a fun fact about myself
+print("I love playing football.")
+#Performing and printing a simple calculation
+print("The sum of 6 and 2 is:", 6 + 2)
+print("The multiplication of 6 and  2 is:", 6 * 2)
+print("The subtraction of 10 and 5 is:", 10 - 5)
+print("The division of 10 and 5 is:",10/5)
